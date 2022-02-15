5ce817a099102850aea41ac8f09256ad9170545d
Kuldeep Kaira <96609348+KuldeepKaira@users.noreply.github.com>
2022-02-15 12:01:15 +0530

    Add files via upload

---
+++ QRCode_Generator/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import QRCode from "./QRCode/qrcode.js";
+
+function App() {
+  return (
+    <div className="App">
+      <QRCode />
+    </div>
+  );
+}
+
+export default App;
